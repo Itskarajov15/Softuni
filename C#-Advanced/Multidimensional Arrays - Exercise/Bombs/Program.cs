eb40e7c164bcd63b558d48f79df6dbb3288e458d
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-02 20:30:08 +0300

    Added Bombs and Miner

---
+++ C#-Advanced/Multidimensional Arrays - Exercise/Bombs/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            FillingTheMatrix(matrix);
+
+            List<string> bombs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Explosion(bombs, matrix);
+
+            int aliveCells = 0;
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCells++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sum}");
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Explosion(List<string> bombs, int[,] matrix)
+        {
+            for (int i = 0; i < bombs.Count; i++)
+            {
+                List<int> currBomb = bombs[i]
+                        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+
+                int bombRow = currBomb[0];
+                int bombCol = currBomb[1];
+
+                int bombPower = matrix[bombRow, bombCol];
+
+                if (matrix[bombRow, bombCol] <= 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (IsToExplode(bombRow - 1, bombCol - 1, matrix))
+                    {
+                        matrix[bombRow - 1, bombCol - 1] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow - 1, bombCol, matrix))
+                    {
+                        matrix[bombRow - 1, bombCol] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow - 1, bombCol + 1, matrix))
+                    {
+                        matrix[bombRow - 1, bombCol + 1] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow, bombCol - 1, matrix))
+                    {
+                        matrix[bombRow, bombCol - 1] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow, bombCol + 1, matrix))
+                    {
+                        matrix[bombRow, bombCol + 1] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow + 1, bombCol - 1, matrix))
+                    {
+                        matrix[bombRow + 1, bombCol - 1] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow + 1, bombCol, matrix))
+                    {
+                        matrix[bombRow + 1, bombCol] -= bombPower;
+                    }
+
+                    if (IsToExplode(bombRow + 1, bombCol + 1, matrix))
+                    {
+                        matrix[bombRow + 1, bombCol + 1] -= bombPower;
+                    }
+
+                    matrix[bombRow, bombCol] = 0;
+                }
+
+            }
+        }
+
+        static int[] ReadIntArrayFromTheConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        static void FillingTheMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = ReadIntArrayFromTheConsole();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+        }
+
+        static bool IsToExplode(int bombRow, int bombCol, int[,] matrix)
+        {
+            return bombRow >= 0 && bombRow < matrix.GetLength(0) && bombCol >= 0 && bombCol < matrix.GetLength(1) && matrix[bombRow, bombCol] > 0;
+        }
+    }
+}
---
+++ C#-Advanced/Multidimensional Arrays - Exercise/Miner/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Linq;
+
+namespace Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[fieldSize, fieldSize];
+
+            string[] commands = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int amountOfCoalOnTheField = 0;
+            int collectedCoal = 0;
+            int[] currPosition = new int[2];
+
+            amountOfCoalOnTheField = FillingTheField(field, amountOfCoalOnTheField, currPosition);
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string direction = commands[i];
+
+                if (direction == "left")
+                {
+                    currPosition = MovesTheMiner(field, direction, currPosition);
+                }
+                else if (direction == "right")
+                {
+                    currPosition = MovesTheMiner(field, direction, currPosition);
+
+                }
+                else if (direction == "up")
+                {
+                    currPosition = MovesTheMiner(field, direction, currPosition);
+                }
+                else if (direction == "down")
+                {
+                    currPosition = MovesTheMiner(field, direction, currPosition);
+                }
+
+                char symbolAtCurrPosition = field[currPosition[0], currPosition[1]];
+
+                if (symbolAtCurrPosition == 'e')
+                {
+                    Console.WriteLine($"Game over! ({currPosition[0]}, {currPosition[1]})");
+                    return;
+                }
+                else if (symbolAtCurrPosition == 'c')
+                {
+                    amountOfCoalOnTheField--;
+                    collectedCoal++;
+
+                    if (amountOfCoalOnTheField <= 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({currPosition[0]}, {currPosition[1]})");
+                        return;
+                    }
+                    else
+                    {
+                        field[currPosition[0], currPosition[1]] = '*';
+                    }
+                }
+            }
+
+            Console.WriteLine($"{amountOfCoalOnTheField} coals left. ({currPosition[0]}, {currPosition[1]})");
+        }
+
+        static int[] MovesTheMiner(char[,] field, string direction, int[] currPosition)
+        {
+            int row = currPosition[0];
+            int col = currPosition[1];
+
+            if (direction == "left")
+            {
+                if (col - 1 >= 0)
+                {
+                    col -= 1;
+                }
+            }
+            else if (direction == "right")
+            {
+                if (col + 1 < field.GetLength(1))
+                {
+                    col += 1;
+                }
+            }
+            else if (direction == "up")
+            {
+                if (row - 1 >= 0)
+                {
+                    row -= 1;
+                }
+            }
+            else if (direction == "down")
+            {
+                if (row + 1 < field.GetLength(0))
+                {
+                    row += 1;
+                }
+            }
+
+            currPosition[0] = row;
+            currPosition[1] = col;
+
+            return currPosition;
+        }
+
+        static int FillingTheField(char[,] field, int amountOfCoalOnTheField, int[] currPosition)
+        {
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                char[] currRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    if (currRow[col] == 'c')
+                    {
+                        amountOfCoalOnTheField++;
+                    }
+
+                    if (currRow[col] == 's')
+                    {
+                        currPosition[0] = row;
+                        currPosition[1] = col;
+                    }
+
+                    field[row, col] = currRow[col];
+                }
+            }
+
+            return amountOfCoalOnTheField;
+        }
+    }
+}
