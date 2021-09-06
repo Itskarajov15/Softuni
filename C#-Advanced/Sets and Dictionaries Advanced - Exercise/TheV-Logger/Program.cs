ceed6dd261d7e0590d299ff6ccf97ea0d6d6174d
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-06 19:00:30 +0300

    Added new exercises

---
+++ C#-Advanced/Sets and Dictionaries Advanced - Exercise/TheV-Logger/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheV_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, SortedSet<string>>> app = new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "Statistics")
+            {
+                string vlogger = command[0];
+
+                if (command[1] == "joined")
+                {
+                    if (!app.ContainsKey(vlogger))
+                    {
+                        app.Add(vlogger, new Dictionary<string, SortedSet<string>>());
+
+                        app[vlogger].Add("following", new SortedSet<string>());
+                        app[vlogger].Add("followers", new SortedSet<string>());
+                    }
+                }
+                else if (command[1] == "followed")
+                {
+                    string firstVlogger = command[0];
+                    string secondVlogger = command[2];
+
+                    if ((app.ContainsKey(firstVlogger) && app.ContainsKey(secondVlogger))
+                        && (firstVlogger != secondVlogger)
+                        && !app[firstVlogger]["following"].Contains(secondVlogger))
+                    {
+                        app[firstVlogger]["following"].Add(secondVlogger);
+                        app[secondVlogger]["followers"].Add(firstVlogger);
+                    }
+                    else
+                    {
+                        command = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        continue;
+                    }
+                }
+
+                command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            app = app
+                .OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine($"The V-Logger has a total of {app.Count} vloggers in its logs.");
+
+            int counter = 1;
+
+            foreach (var vlogger in app)
+            {
+                Console.WriteLine($"{counter}. {vlogger.Key} : {vlogger.Value["followers"].Count} followers, {vlogger.Value["following"].Count} following");
+
+                if (counter == 1 && vlogger.Value["followers"].Count > 0)
+                {
+                    foreach (var follower in vlogger.Value["followers"])
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+
+                counter++;
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Sets and Dictionaries Advanced - Exercise/Wardrobe/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                FillingTheWardrobe(wardrobe, input);
+            }
+
+            string[] searchedItem = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var clothes in color.Value)
+                {
+                    if (searchedItem[0] == color.Key && searchedItem[1] == clothes.Key)
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothes.Key} - {clothes.Value}");
+                    }
+                }
+            }
+        }
+        static void FillingTheWardrobe(Dictionary<string, Dictionary<string, int>> wardrobe, string[] input)
+        {
+            string color = input[0];
+
+            for (int j = 1; j < input.Length; j++)
+            {
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                if (!wardrobe[color].ContainsKey(input[j]))
+                {
+                    wardrobe[color].Add(input[j], 0);
+                }
+
+                wardrobe[color][input[j]]++;
+            }
+        }
+    }
+}
