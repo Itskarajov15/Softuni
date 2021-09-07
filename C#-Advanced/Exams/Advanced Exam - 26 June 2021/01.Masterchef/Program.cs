10ad50d21d8f1686830c67d390ae8408671164e7
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-07 13:19:36 +0300

    Added Exams.

---
+++ C#-Advanced/Exams/Advanced Exam - 26 June 2021/01.Masterchef/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, int> dishes = new SortedDictionary<string, int>();
+
+            int[] sequenceOfIngredients = ReadIntArrayFromTheConsole();
+
+            int[] sequenceOfFreshnessValues = ReadIntArrayFromTheConsole();
+
+            Queue<int> ingredients = new Queue<int>(sequenceOfIngredients);
+            Stack<int> freshnessValues = new Stack<int>(sequenceOfFreshnessValues);
+
+            while (ingredients.Count > 0 && freshnessValues.Count > 0)
+            {
+                int ingredient = ingredients.Dequeue();
+
+                if (ingredient == 0)
+                {
+                    continue;
+                }
+
+                int freshnessValue = freshnessValues.Pop();
+
+                int sum = ingredient * freshnessValue;
+
+                if (sum == 150)
+                {
+                    string dish = "Dipping sauce";
+
+                    AddTheDishInTheDict(dish, dishes);
+                }
+                else if (sum == 250)
+                {
+                    string dish = "Green salad";
+
+                    AddTheDishInTheDict(dish, dishes);
+                }
+                else if (sum == 300)
+                {
+                    string dish = "Chocolate cake";
+
+                    AddTheDishInTheDict(dish, dishes);
+                }
+                else if (sum == 400)
+                {
+                    string dish = "Lobster";
+
+                    AddTheDishInTheDict(dish, dishes);
+                }
+                else
+                {
+                    ingredients.Enqueue(ingredient + 5);
+                }
+            }
+
+            if (dishes.Count >= 4)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes! ");
+            }
+            else
+            {
+                Console.WriteLine($"You were voted off. Better luck next year.");
+
+                if (ingredients.Sum() > 0)
+                {
+                    Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+                }
+            }
+
+            foreach (var dish in dishes)
+            {
+                Console.WriteLine($"# {dish.Key} --> {dish.Value}");
+            }
+        }
+
+        static void AddTheDishInTheDict(string dish, SortedDictionary<string, int> dishes)
+        {
+            if (!dishes.ContainsKey(dish))
+            {
+                dishes.Add(dish, 0);
+            }
+
+            dishes[dish]++;
+        }
+
+        static int[] ReadIntArrayFromTheConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
