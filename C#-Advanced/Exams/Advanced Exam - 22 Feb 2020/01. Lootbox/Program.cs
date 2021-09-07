357d2b61ba16d0d2b47b9af078c11f629194d973
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-07 13:37:40 +0300

    Added Advanced Exam - 22 Feb 2020.

---
+++ C#-Advanced/Exams/Advanced Exam - 22 Feb 2020/01. Lootbox/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstSequence = ReadIntArrayFromTheConsole();
+            int[] secondSequence = ReadIntArrayFromTheConsole();
+
+            Queue<int> firstBox = new Queue<int>(firstSequence);
+            Stack<int> secondBox = new Stack<int>(secondSequence);
+
+            int claimedItems = 0;
+
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                int firstItem = firstBox.Peek();
+                int secondItem = secondBox.Peek();
+
+                int sum = firstItem + secondItem;
+
+                if (sum % 2 == 0)
+                {
+                    claimedItems += sum;
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondItem);
+                    secondBox.Pop();
+                }
+            }
+
+            if (firstBox.Count <= 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (claimedItems >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItems}");
+            }
+        }
+
+        static int[] ReadIntArrayFromTheConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
