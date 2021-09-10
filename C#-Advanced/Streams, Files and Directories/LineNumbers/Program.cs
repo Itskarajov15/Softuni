4bd9ced31eeca21c9bf79e4fe00657ed62ebcbf4
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-10 16:49:37 +0300

    Added Streams, Files and Directories

---
+++ C#-Advanced/Streams, Files and Directories/LineNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader str = new StreamReader("input.txt"))
+            {
+                string line = await str.ReadLineAsync();
+                int counter = 1;
+
+                using (StreamWriter wtr = new StreamWriter("Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        await wtr.WriteLineAsync($"{counter}. {line}");
+
+                        counter++;
+                        line = await str.ReadLineAsync();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Streams, Files and Directories/OddLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using(StreamReader str = new StreamReader("input.txt"))
+            {
+                string line = await str.ReadLineAsync();
+                int counter = 0;
+
+                while(line != null)
+                {
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    counter++;
+                    line = await str.ReadLineAsync();
+                }
+            }
+        }
+    }
+}
