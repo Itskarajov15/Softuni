153599ed21fd7f1537f917163656b79a161d378c
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-11 13:17:37 +0300

    Added new exercises

---
+++ C#-Advanced/Streams, Files and Directories/FolderSize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = Console.ReadLine();
+
+            var files = Directory.GetFiles(folderPath);
+
+            long fileSizes = 0;
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                fileSizes += file.Length;
+            }
+
+            Console.WriteLine(fileSizes / 1024.0);
+        }
+    }
+}
---
+++ C#-Advanced/Streams, Files and Directories/StreamsUnderneath/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace StreamsUnderneath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream stream = new FileStream("../../../students.txt", FileMode.Open))
+            {
+                byte[] buffer = new byte[2];
+                Console.WriteLine($"Stream position {stream.Position}");
+
+                for (int i = 0; i < stream.Length / buffer.Length; i++)
+                {
+                    stream.Read(buffer, 0, 2);
+
+                    using (FileStream writerStream = new FileStream($"../../../students - {i}.txt", FileMode.Create, FileAccess.Write))
+                    {
+                        writerStream.Write(buffer, 0, 2);
+                    }
+                }
+
+                Console.WriteLine($"Stream position {stream.Position}");
+            }
+        }
+    }
+}
