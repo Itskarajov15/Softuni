88aefd71a2eb0cfb5bf344621c1cc3c716e10f9b
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-12 15:57:01 +0300

    Added Functional Programming - Lab

---
+++ C#-Advanced/Functional Programming - Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> filer = text => Char.IsUpper(text[0]);
+
+            string text = Console.ReadLine();
+            string[] words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Where(filer)
+                .ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Functional Programming - Lab/SumNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(int.Parse
+                , array => array.Length
+                , array =>
+                {
+                    int sum = 0;
+
+                    foreach (var item in array)
+                    {
+                        sum += item;
+                    }
+
+                    return sum;
+                });
+        }
+
+        static void PrintSumAndCount(Func<string, int> parser,
+            Func<int[], int> countGetter,
+            Func<int[], int> sumGetter)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(countGetter(numbers));
+            Console.WriteLine(sumGetter(numbers));
+        }
+    }
+}
