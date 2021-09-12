c8f761aab8e1f6f42d79cc487ec8f630e9c85f1b
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-09-12 20:42:00 +0300

    Added new exercises in Functional Programming

---
+++ C#-Advanced/Functional Programming - Lab/AddVAT/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToArray();
+
+            prices = Select(prices, x => x * 1.2M);
+
+            foreach (var item in prices)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+
+        static decimal[] Select(decimal[] prices, Func<decimal, decimal> transformer)
+        {
+            decimal[] newArray = new decimal[prices.Length];
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                newArray[i] = transformer(prices[i]);
+            }
+
+            return newArray;
+        }
+    }
+}
