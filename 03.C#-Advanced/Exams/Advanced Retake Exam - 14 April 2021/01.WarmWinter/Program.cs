9bdaf6d5d944ff89207dddc520164dbb9254260c
Asen <asen__7@abv.bg>
2021-09-21 11:23:25 +0300

    Added Advanced Retake Exam - 14 April 2021

---
+++ 03.C#-Advanced/Exams/Advanced Retake Exam - 14 April 2021/01.WarmWinter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sets = new List<int>();
+
+            int[] hats = ReadingArrayFromTheConsole();
+            int[] scarfs = ReadingArrayFromTheConsole();
+
+            var stackOfHats = new Stack<int>(hats);
+            var queueOfScarfs = new Queue<int>(scarfs);
+
+            while (stackOfHats.Count > 0 && queueOfScarfs.Count > 0)
+            {
+                if (stackOfHats.Peek() > queueOfScarfs.Peek())
+                {
+                    sets.Add(stackOfHats.Pop() + queueOfScarfs.Dequeue());
+                }
+                else if (stackOfHats.Peek() == queueOfScarfs.Peek())
+                {
+                    queueOfScarfs.Dequeue();
+                    int currHat = stackOfHats.Pop() + 1;
+
+                    stackOfHats.Push(currHat);
+
+                    for (int i = 0; i < stackOfHats.Count; i++)
+                    {
+                        stackOfHats.Push(stackOfHats.Pop());
+                    }
+                }
+                else if (stackOfHats.Peek() < queueOfScarfs.Peek())
+                {
+                    stackOfHats.Pop();
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.OrderByDescending(x => x).FirstOrDefault()}");
+
+            Console.WriteLine(String.Join(" ", sets));
+        }
+
+        static int[] ReadingArrayFromTheConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ 03.C#-Advanced/Exams/Advanced Retake Exam - 14 April 2021/03. CocktailParty/Cocktail.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.MaxAlcoholLevel = maxAlcoholLevel;
+            this.Ingredients = new List<Ingredient>();
+        }
+
+        public List<Ingredient> Ingredients { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+        public int CurrentAlcoholLevel
+        {
+            get { return this.Ingredients.Sum(x => x.Alcohol); }
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            var isInTheList = Ingredients.Any(x => x.Name == ingredient.Name);
+            var isEnoughAlcohol = ingredient.Alcohol <= this.MaxAlcoholLevel;
+
+            if (!isInTheList && isEnoughAlcohol)
+            {
+                this.Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return Ingredients.Remove(Ingredients.Find(x => x.Name == name));
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return Ingredients.Find(x => x.Name == name);
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {this.Name} - Current Alcohol Level: {this.CurrentAlcoholLevel}");
+
+            foreach (var ingredient in this.Ingredients)
+            {
+                sb.AppendLine($"{ingredient}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
