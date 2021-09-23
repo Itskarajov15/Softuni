490ed37ee0098f2782ab8b5343b0ff6c7325cbc4
Asen <asen__7@abv.bg>
2021-09-23 13:12:06 +0300

    Added Implementing Linked List

---
+++ 03.C#-Advanced/13. Implementing Linked List/LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new SoftuniLinkedList();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                linkedList.AddFirst(i);
+            }
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    linkedList.AddLast(i);
+            //}
+
+            Console.WriteLine($"Count: {linkedList.Count}");
+
+            Console.WriteLine($"Remove first element: {linkedList.RemoveFirst()}");
+            Console.WriteLine($"Remove last element: {linkedList.RemoveLast()}");
+
+            linkedList.Foreach(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ 03.C#-Advanced/13. Implementing Linked List/LinkedList/SoftuniLinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class SoftuniLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            var node = new Node(element);
+
+            Count++;
+
+            if (Head == null)
+            {
+                Head = Tail = node;
+                return;
+            }
+
+            node.Next = Head;
+            Head.Previous = node;
+            Head = node;
+        }
+
+        public void AddLast(int element)
+        {
+            var node = new Node(element);
+
+            Count++;
+
+            if (Tail == null)
+            {
+                Head = Tail = node;
+                return;
+            }
+
+            node.Previous = Tail;
+            Tail.Next = node;
+            Tail = node;
+        }
+
+        public int RemoveFirst()
+        {
+            if (Count <= 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = Head.Value;
+            Head = Head.Next;
+
+            if (Head != null)
+            {
+                Head.Previous = null;
+            }
+            else
+            {
+                Tail = null;
+            }
+
+            Count--;
+
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (Count <= 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = Tail.Value;
+            Tail = Tail.Previous;
+
+            if (Tail != null)
+            {
+                Tail.Next = null;
+            }
+            else
+            {
+                Head = null;
+            }
+
+            Count--;
+
+            return lastElement;
+        }
+
+        public void Foreach(Action<int> action)
+        {
+            var currNode = Head;
+
+            while (currNode != null)
+            {
+                action(currNode.Value);
+                currNode = currNode.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            var array = new int[Count];
+            var counter = 0;
+
+            var currNode = Head;
+
+            while (currNode != null)
+            {
+                array[counter] = currNode.Value;
+                currNode = currNode.Next;
+                counter++;
+            }
+
+            return array;
+        }
+    }
+}
