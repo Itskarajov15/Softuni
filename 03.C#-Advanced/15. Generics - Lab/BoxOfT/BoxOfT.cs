8c111d07df7c558fc5ce2f91994b158715931c8f
Asen <asen__7@abv.bg>
2021-09-25 23:45:39 +0300

    Added Generics

---
+++ 03.C#-Advanced/15. Generics - Lab/BoxOfT/BoxOfT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        List<T> elements;
+
+        public BoxOfT()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Count
+        {
+            get => this.elements.Count;
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            var itemToRemove = this.elements.Last();
+
+            this.elements.RemoveAt(this.elements.Count - 1);
+
+            return itemToRemove;
+        }
+    }
+}
---
+++ 03.C#-Advanced/15. Generics - Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = ArrayCreator.Create(5, "Pesho");
+
+            foreach (var item in stringArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
