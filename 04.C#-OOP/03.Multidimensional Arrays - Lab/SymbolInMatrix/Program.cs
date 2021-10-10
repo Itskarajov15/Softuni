df3bf3173783b7dfb7209c1a61a49c978cd0faa6
Asen <asen__7@abv.bg>
2021-10-10 16:19:22 +0300

    asd

---
+++ 04.C#-OOP/03.Multidimensional Arrays - Lab/SymbolInMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            bool isFound = false;
+
+            char[,] matrix = new char[sizeOfMatrix, sizeOfMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string text = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = text[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (isFound)
+                {
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
