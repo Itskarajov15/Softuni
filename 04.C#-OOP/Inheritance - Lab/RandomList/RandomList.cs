1f2a401fa5afa4b415eccbd42b42f153ee676db1
Asen <asen__7@abv.bg>
2021-10-10 16:17:53 +0300

    Added OOP folder and Inheritance - Lab.sln

---
+++ 04.C#-OOP/Inheritance - Lab/RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private List<string> list;
+
+        public RandomList()
+        {
+            this.list = new List<string>();
+        }
+
+        public string RandomString()
+        {
+            Random rand = new Random();
+            int index = rand.Next(0, list.Count);
+
+            string stringToRemove = list[index];
+            list.RemoveAt(index);
+
+            return stringToRemove;
+        }
+    }
+}
