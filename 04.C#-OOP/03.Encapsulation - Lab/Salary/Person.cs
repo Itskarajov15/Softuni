f6e73173ba824754a3742584da4f6e0f2ac0192c
Asen <asen__7@abv.bg>
2021-10-12 14:54:18 +0300

    Added Encapsulation - Lab

---
+++ 04.C#-OOP/03.Encapsulation - Lab/Salary/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (IsYoungerThanThirty(this.Age))
+            {
+                percentage /= 2;
+            }
+
+            this.Salary += (this.Salary * (percentage / 100M));
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        private bool IsYoungerThanThirty(int age)
+        {
+            return this.Age < 30;
+        }
+    }
+}
---
+++ 04.C#-OOP/03.Encapsulation - Lab/Team/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArg = Console.ReadLine()
+                    .Split();
+
+                var person = new Person(cmdArg[0], cmdArg[1], int.Parse(cmdArg[2]), decimal.Parse(cmdArg[3]));
+
+                persons.Add(person);
+            }
+
+            var team = new Team("Softuni");
+
+            foreach (var person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
