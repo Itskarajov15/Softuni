74a4965c6f5a1690466498379ac5161020045fdd
Asen <asen__7@abv.bg>
2021-10-12 10:57:23 +0300

    Added Animals exercise

---
+++ 04.C#-OOP/02.Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return "Produce sound";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine(ProduceSound());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- 04.C#-OOP/02.Inheritance - Exercise/Animals/StartUp.cs
+++ 04.C#-OOP/02.Inheritance - Exercise/Animals/StartUp.cs
@@ -1,10 +1,54 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> listOfAnimals = new List<Animal>();
+
+            while (true)
+            {
+                string typeOfAnimal = Console.ReadLine();
+
+                if (typeOfAnimal == "Beast!")
+                {
+                    break;
+                }
+
+                var infoAboutTheAnimal = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = infoAboutTheAnimal[0];
+                int age = int.Parse(infoAboutTheAnimal[1]);
+                string gender = infoAboutTheAnimal[2];
+
+                if (String.IsNullOrEmpty(name) || age <= 0 || String.IsNullOrEmpty(gender))
+                {
+                    continue;
+                }
+
+                switch (typeOfAnimal)
+                {
+                    case "Dog": listOfAnimals.Add(new Dog(name, age, gender));
+                        break;
+                    case "Cat": listOfAnimals.Add(new Cat(name, age, gender));
+                        break;
+                    case "Frog": listOfAnimals.Add(new Frog(name, age, gender));
+                        break;
+                    case "Kitten": listOfAnimals.Add(new Kitten(name, age));
+                        break;
+                    case "Tomcat": listOfAnimals.Add(new Tomcat(name, age));
+                        break;
+                }
+            }
+
+            foreach (var animal in listOfAnimals)
+            {
+                Console.WriteLine(animal);
+            }
         }
     }
 }
