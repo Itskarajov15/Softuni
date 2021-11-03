b494421b488a274ef2a863f117f64cdd627239ae
Asen <asen__7@abv.bg>
2021-11-03 21:53:54 +0200

    Added Interfaces and Abstraction - Exercise

---
+++ 04.C#-OOP/06.Interfaces and Abstraction - Exercise/Telephony/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var urls = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in numbers)
+            {
+                try
+                {
+                    if (!number.All(char.IsDigit))
+                    {
+                        throw new ArgumentException("Invalid number!");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                if (number.Length == 10)
+                {
+                    var smartphone = new Smartphone();
+
+                    smartphone.Call(number);
+                }
+                else if (number.Length == 7)
+                {
+                    var stationaryPhone = new StationaryPhone();
+
+                    stationaryPhone.Call(number);
+                }
+            }
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    if (url.Any(char.IsDigit))
+                    {
+                        throw new ArgumentException("Invalid URL!");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                var smartphone = new Smartphone();
+
+                smartphone.Browse(url);
+            }
+        }
+    }
+}
