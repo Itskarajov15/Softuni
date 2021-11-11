0b34363e58e8ec810b8e9495f873ebb363be2835
Asen <asen__7@abv.bg>
2021-11-11 16:57:22 +0200

    Added Raiding to Polymorphism - Exercise

---
+++ 04.C#-OOP/08.Polymorphism - Exercise/Raiding/HeroFactory.cs
@@ -0,0 +1,34 @@
+﻿using Raiding.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class HeroFactory
+    {
+        public BaseHero CreateHero(string name, string type)
+        {
+            if (type == nameof(Druid))
+            {
+                return new Druid(name);
+            }
+            else if (type == nameof(Paladin))
+            {
+                return new Paladin(name);
+            }
+            else if (type == nameof(Rogue))
+            {
+                return new Rogue(name);
+            }
+            else if (type == nameof(Warrior))
+            {
+                return new Warrior(name);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ 04.C#-OOP/08.Polymorphism - Exercise/Raiding/Models/BaseHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Models
+{
+    public abstract class BaseHero
+    {
+        protected BaseHero(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public virtual int Power { get; set; }
+
+        public abstract string CastAbility();
+    }
+}
