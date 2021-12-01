a1d2ec87587c2c285e553cf9b0a61f58bd42f04a
Asen <asen__7@abv.bg>
2021-12-01 23:32:13 +0200

    Added Exams

---
+++ 04.C#-OOP/Exams/C# OOP Retake Exam - 18 April 2021/Repositories/BunnyRepository.cs
@@ -0,0 +1,28 @@
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Easter.Repositories
+{
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private List<IBunny> bunnies;
+
+        public BunnyRepository()
+        {
+            this.bunnies = new List<IBunny>();
+        }
+
+        public IReadOnlyCollection<IBunny> Models => this.bunnies.AsReadOnly();
+
+        public void Add(IBunny model)
+            => this.bunnies.Add(model);
+
+        public IBunny FindByName(string name)
+            => this.bunnies.FirstOrDefault(b => b.Name == name);
+
+        public bool Remove(IBunny model)
+            => this.bunnies.Remove(model);
+    }
+}
