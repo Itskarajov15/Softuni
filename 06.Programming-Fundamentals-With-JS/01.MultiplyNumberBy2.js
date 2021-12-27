eca0212b0175bf6e1b7b8bbfd5c0da25812bda1e
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2021-12-27 12:30:29 +0200

    Added JS fundamentals

---
+++ 06.Programming-Fundamentals-With-JS/01.MultiplyNumberBy2.js
@@ -0,0 +1,5 @@
+function multiplyNumbersBy2(number) {
+    console.log(number * 2);
+}
+
+multiplyNumbersBy2(2);
---
+++ 06.Programming-Fundamentals-With-JS/06.StudentInformation.js
@@ -0,0 +1,5 @@
+function studentInformation(name, age, averageGrade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`)
+}
+
+studentInformation("Lelq ti Goshka", 15, 5.9999999999);
