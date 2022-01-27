1aa7222bb79a571b3d7182065a887c39d707d6e2
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2022-01-27 18:49:33 +0200

    Added solution to 08. Generate Report

--- 07.JS-Advanced/08.DOM Introduction - Exercise/08. Generate Report/report.js
+++ 07.JS-Advanced/08.DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,33 @@
 function generateReport() {
-    //TODO
+    let headRowArray = Array.from(document.getElementsByTagName('input'));
+    let bodyRowArray = Array.from(document.querySelectorAll('tbody tr'));
+
+    let textArea = document.getElementById('output');
+
+    let indices = [];
+    let employees = [];
+    
+    for (let i = 0; i < headRowArray.length; i++) {
+        if (headRowArray[i].checked) {
+            indices.push(i);
+        }
+    }
+
+    for (let i = 0; i < bodyRowArray.length; i++) {
+        let tdArray = Array.from(bodyRowArray[i].children);
+        let result = {};
+
+        for (let j = 0; j < indices.length; j++) {
+            let key = headRowArray[indices[j]].name.toLowerCase();
+            let value = tdArray[indices[j]].textContent;
+
+            result[key] = value;
+        }
+
+        employees.push(result);
+    }
+
+    let str = JSON.stringify(employees);
+
+    textArea.textContent = str;
 }
