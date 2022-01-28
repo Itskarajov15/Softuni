bf82f00f275f4c308aa77901aaf26945ecf23bc5
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2022-01-28 20:42:05 +0200

    Added new solutions in DOM Manipulation and Events - Lab

--- 07.JS-Advanced/09.DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ 07.JS-Advanced/09.DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = Array.from(document.querySelectorAll('input[type="text"]'));
+
+    inputElements.forEach(element => {
+        element.addEventListener('input', (e) => {
+            let parent = e.target.parentNode;
+            
+            if (e.target.value.length <= 0) {
+                parent.classList.remove('focused');
+            } else {
+                parent.classList.add('focused');
+            }
+        });
+    });
 }
--- 07.JS-Advanced/09.DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ 07.JS-Advanced/09.DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+   let textAreaElement = document.querySelector('textarea');
+
+   let uniqueProducts = [];
+
+   document.querySelector('.shopping-cart').addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON' && e.target.className === 'add-product') {
+         const productName = e.target.parentNode.parentNode.querySelector('.product-title').textContent;
+         const productPrice = Number(e.target.parentNode.parentNode.querySelector('.product-line-price').textContent);
+
+         uniqueProducts.push({ productName, productPrice });
+
+         textAreaElement.value += `Added ${productName} for ${(productPrice).toFixed(2)} to the cart.\n`;
+      }
+   });
+
+   document.querySelector('.checkout').addEventListener('click', (e) => {
+      const result = uniqueProducts.reduce((acc, c) => {
+         acc.products.push(c.productName);
+         acc.totalPrice += c.productPrice;
+         return acc;
+      }, {products: [], totalPrice: 0});
+
+      textAreaElement.value += `You bought ${result.products.join(', ')} for ${result.totalPrice.toFixed(2)}.`;
+
+      Array.from(document.querySelectorAll('button')).forEach(b => b.disabled = true);
+   });
 }
