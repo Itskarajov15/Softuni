96fde1ffc701e9a2a2bc5d1a9dc7a8077acbb02c
Asen <asen__7@abv.bg>
2022-02-01 19:22:01 +0200

    Added new solutions to Advanced Functions - Lab

--- 07.JS-Advanced/11.Advanced Functions - Lab/06.ListProcessor.js
+++ 07.JS-Advanced/11.Advanced Functions - Lab/06.ListProcessor.js
@@ -1,19 +1,35 @@
 function solve(input) {
     let result = [];
-
-    return {
-        add: function(string) {
-            result.push(string);
-        },
-
-        remove: function(string) {
-            result = result
-                        .filter(x => x !== string);
-        },
-
-        print: function() {
-            console.log(result.join(','));
-        }
+    
+    const obj = {
+        add,
+        remove,
+        print
     }
+
+    function add(string) {
+        result.push(string);
+    }
+
+    function remove(string){
+        result = result.filter(x => x !== string);
+    }
+
+    function print(){
+        console.log(result.join(','));
+    }
+
+    input.forEach(i => {
+        let [command, text] = i.split(' ');
+
+        if (command === 'add') {
+            obj.add(text);
+        } else if (command === 'remove') {
+            obj.remove(text);
+        } else if (command === 'print') {
+            obj.print();
+        }
+    })
 }
 
+solve(['add hello', 'add again', 'remove hello', 'add again', 'print']);
