587bf3a1f0b82be588fafacf779b3b9e0ca996de
Itskarajov15 <89154014+Itskarajov15@users.noreply.github.com>
2022-03-07 15:19:50 +0200

    Added new task solution

--- 08.JS-Applications/08.Remote Data and Authentication - Exercise/05.Fisher-Game/src/login.js
+++ 08.JS-Applications/08.Remote Data and Authentication - Exercise/05.Fisher-Game/src/login.js
@@ -1,6 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const formElement = document.querySelector('form');
-    formElement.addEventListener('submit', login);
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', login);
+
+const logoutElement = document.getElementById('logout');
+logoutElement.addEventListener('click', async () => {
+    const url = 'http://localhost:3030/users/logout';
+    
+    const res = await fetch(url);
+    if (res.ok) {
+        sessionStorage.clear();
+        window.location = `index.html`;
+    }
+
+    console.log('yes');
 });
 
 async function login(e) {
@@ -8,31 +19,41 @@ async function login(e) {
 
     const url = 'http://localhost:3030/users/login';
     const formData = new FormData(e.currentTarget);
-    let email = formData.get('email');
-    let password = formData.get('password');
+    const email = formData.get('email');
+    const password = formData.get('password');
 
     try {
         const res = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({email, password}) 
+            body: JSON.stringify({ email, password })
         });
-    
+
         if (res.ok != true) {
-            const error = await res.json();
-            throw new Error(error.message);   
+            const err = await res.json();
+            throw new Error(err.message);
         }
 
         const data = await res.json();
-        const user = {
-            email: data.email,
-            accessToken: data.accessToken,
-            id: data._id
-        }
-    
-        sessionStorage.setItem('userData', JSON.stringify(user));
+
+        sessionStorage.setItem('userData', JSON.stringify(data));
         window.location = 'index.html';
+        formElement.reset();
     } catch (error) {
         alert(error.message);
     }
 }
+
+window.onload = () => {
+    if (sessionStorage.userData) {
+        const userObj = JSON.parse(sessionStorage.userData);
+        console.log(userObj);
+        const userSpan = document.getElementById('user-span');
+        userSpan.textContent = userObj.username;
+        const divGuestElement = document.getElementById('guest');
+        divGuestElement.style.display = 'none';
+    } else {
+        const userLogout = document.getElementById('user');
+        userLogout.style.display = 'none';
+    }
+};
--- 08.JS-Applications/08.Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ 08.JS-Applications/08.Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,61 @@
-console.log('TODO:// Implement Register functionality');
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', register);
+
+async function register(e) {
+    e.preventDefault();
+
+    const url = 'http://localhost:3030/users/register';
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const rePass = formData.get('rePass');
+
+    if (!email || !password || !rePass) {
+        alert('All fields must be filled!');
+    } else if (password !== rePass) {
+        alert('Passwords don\'t match');
+    }
+
+    try {
+        const registerRes = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({email, password})
+        });
+    
+        if (registerRes.ok != true) {
+            const error = await registerRes.json();
+            throw new Error(error.message);
+        }
+
+        const data = await registerRes.json();
+        const username = email.split('@')[0];
+
+        const userData = {
+            email,
+            username: username.charAt(0).toUpperCase() + username.slice(1),
+            id: data._id,
+            accessToken: data.accessToken
+        };
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+        window.location = 'index.html';
+        formElement.reset();
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+window.onload = () => {
+    if (sessionStorage.userData) {
+        const userObj = JSON.parse(sessionStorage.userData);
+
+        const userSpan = document.getElementById('user-span');
+        userSpan.textContent = userObj.username;
+        const divGuestElement = document.getElementById('guest');
+        divGuestElement.style.display = 'none';
+    } else {
+        const userLogout = document.getElementById('user');
+        userLogout.style.display = 'none';
+    }
+};
