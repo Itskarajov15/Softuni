3ffbf6509eca9400fc61107b456b71fc41036ea9
Asen <asen__7@abv.bg>
2022-03-16 20:33:37 +0200

    asd

--- 08.JS-Applications/12.Architecture and Testing - Exercise/03.SoftTerest/src/views/login.js
+++
@@ -1,5 +0,0 @@
-const section = document.getElementById('loginPage');
-
-export function showLogin(context) {
-    context.showSection(section);
-}
--- 08.JS-Applications/12.Architecture and Testing - Exercise/03.SoftTerest/src/views/register.js
+++
@@ -1,5 +0,0 @@
-const section = document.getElementById('registerPage');
-
-export function showRegister(context) {
-    context.showSection(section);
-}
