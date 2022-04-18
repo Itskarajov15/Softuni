0fb54a6e2f827f15d1a7eb5526c7ab70612f254c
Itskarajov15 <asen__7@abv.bg>
2022-04-18 21:06:26 +0300

    Updated a file

--- 09.C#-Databases-Basics/04.Built-in Functions/QueriesForGeographyDatabase.sql
+++ 09.C#-Databases-Basics/04.Built-in Functions/QueriesForGeographyDatabase.sql
@@ -10,7 +10,9 @@ SELECT CountryName AS [Country Name], IsoCode AS [ISO Code]
 	WHERE CountryName LIKE '%a%a%a%'
 	ORDER BY IsoCode ASC
 
---
+/*Combine all peak names with all river names, so that the last letter of each peak name is the same as the first letter of its 
+corresponding river name
+*/
 SELECT PeakName, RiverName,
 	LOWER(CONCAT(PeakName, SUBSTRING(RiverName, 2, LEN(RiverName) - 1))) AS Mix
 	FROM Peaks, Rivers
