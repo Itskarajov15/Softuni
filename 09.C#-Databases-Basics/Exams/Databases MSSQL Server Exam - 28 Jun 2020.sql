ffa52588584cce3e03dbdc7a5f0a6774649951f1
Itskarajov15 <asen__7@abv.bg>
2022-05-01 09:41:14 +0300

    Added new task solution to exam 28 Jun 2020

--- 09.C#-Databases-Basics/Exams/Databases MSSQL Server Exam - 28 Jun 2020.sql
+++ 09.C#-Databases-Basics/Exams/Databases MSSQL Server Exam - 28 Jun 2020.sql
@@ -153,3 +153,21 @@ BEGIN
 END
 
 SELECT dbo.udf_GetColonistsCount('Otroyphus')
+
+--
+CREATE PROC usp_ChangeJourneyPurpose(@JourneyId INT, @NewPurpose VARCHAR(50))
+AS
+	IF((SELECT Id FROM Journeys WHERE Id = @JourneyId) IS NULL)
+	BEGIN
+		THROW 50001, 'The journey does not exist!', 1;
+	END
+	
+	IF((SELECT Purpose FROM Journeys WHERE Id = @JourneyId) = @NewPurpose)
+	BEGIN
+		THROW 50002, 'You cannot change the purpose!', 1;
+	END
+
+	UPDATE Journeys
+	SET Purpose = @NewPurpose
+	WHERE Id = @JourneyId
+GO
