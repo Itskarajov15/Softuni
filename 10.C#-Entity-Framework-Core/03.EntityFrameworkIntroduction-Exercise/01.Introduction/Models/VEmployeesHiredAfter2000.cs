2324bf3691709ed210f56d04fcd13a1d33784e24
Itskarajov15 <asen__7@abv.bg>
2022-05-11 20:17:58 +0300

    Added new task solutions

---
+++ 10.C#-Entity-Framework-Core/03.EntityFrameworkIntroduction-Exercise/01.Introduction/Models/VEmployeesHiredAfter2000.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace _01.Introduction.Models
+{
+    public partial class VEmployeesHiredAfter2000
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
