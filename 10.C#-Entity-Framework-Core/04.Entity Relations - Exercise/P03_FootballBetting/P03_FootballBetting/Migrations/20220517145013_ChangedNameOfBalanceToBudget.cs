ff56d341f279b012d2286837f2729f5a92da5d58
Itskarajov15 <asen__7@abv.bg>
2022-05-17 17:59:21 +0300

    Added P03_FootballBetting

---
+++ 10.C#-Entity-Framework-Core/04.Entity Relations - Exercise/P03_FootballBetting/P03_FootballBetting/Migrations/20220517145013_ChangedNameOfBalanceToBudget.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Migrations
+{
+    public partial class ChangedNameOfBalanceToBudget : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Teams");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Budget",
+                table: "Teams",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Budget",
+                table: "Teams");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Teams",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
---
+++ 10.C#-Entity-Framework-Core/04.Entity Relations - Exercise/P03_FootballBetting/P03_FootballBetting/Migrations/20220517145356_ChangedTypeOfAwayTeamGoals.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Migrations
+{
+    public partial class ChangedTypeOfAwayTeamGoals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AwayTeamGoals",
+                table: "Games",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "AwayTeamGoals",
+                table: "Games",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
