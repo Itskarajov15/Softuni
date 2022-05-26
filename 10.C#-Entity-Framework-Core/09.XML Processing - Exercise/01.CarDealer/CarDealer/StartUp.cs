417d7bd442064929ac47562c4d3ffa0ff5809af4
Itskarajov15 <asen__7@abv.bg>
2022-05-26 22:25:57 +0300

    Added 09.XML Processing - Exercise

---
+++ 10.C#-Entity-Framework-Core/09.XML Processing - Exercise/01.CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using CarDealer.Data;
+using System;
+using System.IO;
+using System.Xml;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dbContext = new CarDealerContext();
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            
+        }
+    }
+}
