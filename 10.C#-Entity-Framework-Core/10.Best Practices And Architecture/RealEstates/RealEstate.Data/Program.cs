e9502cc59d567ffa818bb1e5c000123fdacecf7a
Itskarajov15 <asen__7@abv.bg>
2022-05-30 21:07:18 +0300

    Added 10.Best Practices And Architecture

---
+++ 10.C#-Entity-Framework-Core/10.Best Practices And Architecture/RealEstates/RealEstate.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RealEstate.Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
