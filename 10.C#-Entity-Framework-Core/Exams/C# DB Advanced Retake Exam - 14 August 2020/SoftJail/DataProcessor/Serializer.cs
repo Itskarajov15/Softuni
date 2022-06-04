b49347191d8b929650c9e926d8cb2eacfdfb561e
Itskarajov15 <asen__7@abv.bg>
2022-06-04 17:23:10 +0300

    Added new functionalities

--- 10.C#-Entity-Framework-Core/Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
+++ 10.C#-Entity-Framework-Core/Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -2,18 +2,66 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
     using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Text.Json.Serialization;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            throw new NotImplementedException();
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers.Select(x => new
+                    {
+                        OfficerName = x.Officer.FullName,
+                        Department = x.Officer.Department.Name
+                    })
+                    .OrderBy(x => x.OfficerName)
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(p.PrisonerOfficers.Sum(x => x.Officer.Salary).ToString("F2"))
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToList();
+
+            var result = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+
+            return result;
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
         {
-            throw new NotImplementedException();
+            var splittedPrisoners = prisonersNames.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var result = context.Prisoners
+                .Where(p => splittedPrisoners.Contains(p.FullName))
+                .Select(p => new PrisonerOutputModel
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = p.Mails.Select(x => new MessageOutputModel
+                    {
+                        Description = string.Join("", x.Description.Reverse())
+                    })
+                    .ToArray()
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToList();
+
+            var xml = XmlConverter.Serialize(result, "Prisoners");
+
+            return xml;
         }
     }
 }
