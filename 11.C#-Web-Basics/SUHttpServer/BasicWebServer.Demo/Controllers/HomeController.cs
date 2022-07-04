5a954bce7b2bfa1e52bc7f72bf67c7cd6f5c5936
Itskarajov15 <asen__7@abv.bg>
2022-07-04 18:17:46 +0300

    Added views

--- 11.C#-Web-Basics/SUHttpServer/BasicWebServer.Demo/Controllers/HomeController.cs
+++ 11.C#-Web-Basics/SUHttpServer/BasicWebServer.Demo/Controllers/HomeController.cs
@@ -13,16 +13,6 @@ namespace BasicWebServer.Demo.Controllers
     {
         private const string FileName = "content.txt";
 
-        private const string HtmlForm = @"<form action='/HTML' method='POST'>
-            Name: <input type='text' name='Name'/>
-            Age: <input type='number' name ='Age'/>
-            <input type='submit' value ='Save' />
-        </form>";
-
-        private const string DownloadForm = @"<form action='/Content' method='POST'>
-                <input type='submit' value ='Download Sites Content' /> 
-            </form>";
-
         public HomeController(Request request)
             : base(request)
         {
@@ -33,7 +23,7 @@ namespace BasicWebServer.Demo.Controllers
 
         public Response Redirect() => Redirect("https://softuni.org");
 
-        public Response Html() => Html(HtmlForm);
+        public Response Html() => View();
 
         public Response HtmlFormPost()
         {
@@ -48,7 +38,7 @@ namespace BasicWebServer.Demo.Controllers
             return Text(formData);
         }
 
-        public Response Content() => Html(DownloadForm);
+        public Response Content() => View();
 
         public Response DownloadContent() => File(FileName);
 
