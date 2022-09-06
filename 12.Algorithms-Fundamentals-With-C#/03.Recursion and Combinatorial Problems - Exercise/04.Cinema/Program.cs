09d3abf8d33289401deb8362373cd32f311da78d
Itskarajov15 <asen__7@abv.bg>
2022-09-06 11:55:16 +0300

    Added new exercises int Recursion and Combinatorial Problems - Exercise

--- 12.Algorithms-Fundamentals-With-C#/03.Recursion and Combinatorial Problems - Exercise/04.Cinema/Program.cs
+++ 12.Algorithms-Fundamentals-With-C#/03.Recursion and Combinatorial Problems - Exercise/04.Cinema/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace _04.Cinema
 {
     internal class Program
     {
+        private static List<string> nonStaticPeople;
+        private static string[] people;
+        private static bool[] locked;
+
         static void Main(string[] args)
         {
-            var names = Console.ReadLine()
+            nonStaticPeople = Console.ReadLine()
                                .Split(", ")
                                .ToList();
+
+            people = new string[nonStaticPeople.Count];
+            locked = new bool[nonStaticPeople.Count];
 
             var input = Console.ReadLine();
 
@@ -17,8 +26,59 @@ namespace _04.Cinema
             {
                 var splitedInput = input.Split(" - ").ToArray();
 
+                var name = splitedInput[0];
+                var position = int.Parse(splitedInput[1]) - 1;
+
+                people[position] = name;
+                locked[position] = true;
+
+                nonStaticPeople.Remove(name);
+
                 input = Console.ReadLine();
             }
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= nonStaticPeople.Count)
+            {
+                Print();
+                return;
+            }
+
+            Permute(idx + 1);
+
+            for (int i = idx + 1; i < nonStaticPeople.Count; i++)
+            {
+                Swap(idx, i);
+                Permute(idx + 1);
+                Swap(idx, i);
+            }
+        }
+
+        private static void Print()
+        {
+            var peopleIdx = 0;
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (!locked[i])
+                {
+                    people[i] = nonStaticPeople[peopleIdx++];
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", people));
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = nonStaticPeople[first];
+            nonStaticPeople[first] = nonStaticPeople[second];
+            nonStaticPeople[second] = temp;
         }
     }
 }
---
+++ 12.Algorithms-Fundamentals-With-C#/03.Recursion and Combinatorial Problems - Exercise/05.SchoolTeams/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SchoolTeams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = Console.ReadLine()
+                               .Split(", ")
+                               .ToArray();
+
+            var girlsComb = new string[3];
+            var girlsCombs = new List<string[]>();
+
+            GenCombs(0, 0, girls, girlsComb, girlsCombs);
+
+            var boys = Console.ReadLine()
+                   .Split(", ")
+                   .ToArray();
+
+            var boysComb = new string[2];
+            var boysCombs = new List<string[]>();
+
+            GenCombs(0, 0, boys, boysComb, boysCombs);
+
+            PrintFinalCombs(girlsCombs, boysCombs);
+        }
+
+        private static void PrintFinalCombs(List<string[]> girlsCombs, List<string[]> boysCombs)
+        {
+            foreach (var girlsComb in girlsCombs)
+            {
+                foreach (var boysComb in boysCombs)
+                {
+                    Console.WriteLine($"{string.Join(", ", girlsComb)}, {string.Join(", ", boysComb)}");
+                }
+            }
+        }
+
+        private static void GenCombs(int idx, int start, string[] elements, string[] comb, List<string[]> combs)
+        {
+            if (idx >= comb.Length)
+            {
+                combs.Add(comb.ToArray());
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                comb[idx] = elements[i];
+                GenCombs(idx + 1, i + 1, elements, comb, combs);
+            }
+        }
+    }
+}
