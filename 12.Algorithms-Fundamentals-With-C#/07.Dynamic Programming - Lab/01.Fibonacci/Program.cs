14813b40d5cfd6b0c441b2d8d7982887c38aff5d
Itskarajov15 <asen__7@abv.bg>
2022-09-12 19:36:27 +0300

    Added 07.Dynamic Programming - Lab

---
+++ 12.Algorithms-Fundamentals-With-C#/07.Dynamic Programming - Lab/01.Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Fibonacci
+{
+    internal class Program
+    {
+        private static Dictionary<int, long> cache;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            cache = new Dictionary<int, long>();
+
+            Console.WriteLine(FindFib(n));
+        }
+
+        private static long FindFib(int n)
+        {
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+
+            if (n < 2)
+            {
+                return n;
+            }
+
+            var result = FindFib(n - 1) + FindFib(n - 2);
+
+            cache[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ 12.Algorithms-Fundamentals-With-C#/07.Dynamic Programming - Lab/02.SubsetSum/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SubsetSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 3, 5, 1, 4, 2 };
+            var target = 6;
+
+            var possibleSums = GetAllPossibleSums(nums);
+
+            if (possibleSums.ContainsKey(target))
+            {
+                var includedNums = FindSubset(possibleSums, target);
+
+                Console.WriteLine(String.Join(" ", includedNums));
+            }
+            else
+            {
+                Console.WriteLine("Sum was not possible");
+            }
+        }
+
+        private static List<int> FindSubset(Dictionary<int, int> possibleSums,int target)
+        {
+            var subset = new List<int>();
+
+            while (target > 0)
+            {
+                var num = possibleSums[target];
+                subset.Add(num);
+                target -= num;
+            }
+
+            return subset;
+        }
+
+        private static Dictionary<int, int> GetAllPossibleSums(int[] nums)
+        {
+            var sums = new Dictionary<int, int>() { { 0, 0 } };
+
+            foreach (var num in nums)
+            {
+                var currSums = sums.Keys.ToArray();
+
+                foreach (var sum in currSums)
+                {
+                    var newSum = sum + num;
+
+                    if (sums.ContainsKey(newSum))
+                    {
+                        continue;
+                    }
+
+                    sums.Add(newSum, num);
+                }
+            }
+
+            return sums;
+        }
+    }
+}
